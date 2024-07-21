e1c461378cccd90430fcba87081b6407575a21c5
bisht-xp <bishtkamal2907@gmail.com>
2024-07-21 22:41:49 +0530

    create: created hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,67 @@
+"use client";
+
+import ArrowIcon from "@/public/assets/arrow-right.svg";
+import cogImage from "@/public/assets/cog.png";
+import clyinderImage from "@/public/assets/cylinder.png";
+import noodleImage from "@/public/assets/noodle.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
 export const Hero = () => {
-  return null;
+  return (
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+      <div className="common-container">
+        <div className="md:flex items-center justify-between">
+          <div className="md:w-[478px] lg:w-1/2">
+            <div className="tag">Version 2.0 is here</div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+              Pathways to productivity
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivates your efforts, and clebreate your
+              successess.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
+          </div>
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <motion.img
+              src={cogImage.src}
+              alt="Cog Image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:left-4 lg:left-20"
+              animate={{
+                translateY:[-30,30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut"
+              }}
+            />
+            <Image
+              src={clyinderImage}
+              alt="Clyinder Image"
+              width={220}
+              height={220}
+              className="hidden md:block -top-10 -left-28 md:absolute"
+            />
+            <Image
+              src={noodleImage}
+              alt="Noodle Image"
+              width={220}
+              height={220}
+              className="hidden lg:block top-[520px] left-[538px] absolute rotate-[30deg]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
