e8850ecd6aeab6a669c609ea0d735e7408395bd9
bisht-xp <bishtkamal2907@gmail.com>
2024-07-21 22:42:42 +0530

    create: created logoTicker section

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -1,3 +1,50 @@
+import acmeLogo from "@/public/assets/logo-acme.png";
+import apexLogo from "@/public/assets/logo-apex.png";
+import celestialLogo from "@/public/assets/logo-celestial.png";
+import echoLogo from "@/public/assets/logo-echo.png";
+import pulseLogo from "@/public/assets/logo-pulse.png";
+import quantumLogo from "@/public/assets/logo-quantum.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="common-container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={acmeLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="Apex Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="Celestial Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="Echo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="Pulse Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="Quantum Logo"
+              className="logo-ticker-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
